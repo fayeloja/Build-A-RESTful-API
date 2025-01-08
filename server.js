fae39ae15cf38571237993c452224cc3df81d12e
fayeloja <fayeloja@gmail.com>
2025-01-08 11:21:42 +0100

    Added DB Update and Delete Features

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/productModel')
 
-//express app
+//Instance of Express app
 const app = express()
 
 //middleware
-app.use(express.json()) // for parsing application/json
+app.use(express.json()) // for parsing application/json 
+
+app.use(express.urlencoded({extended: false})) // for parsing form encoded data
 
 //port
 const port = 3000
@@ -20,11 +22,38 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Abiola!')
   })
 
+// Fetch all products from DB
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }   
+})  
 
+// Fetch a single product from DB
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }   
+})
+
+
+// Create a product in DB
 app.post('/product', async (req, res) => {
         try {
             const product = await Product.create(req.body)
-            res.status(200).json(product)
+            res.status(201).json(product)
             
         } catch (error) {
             console.log(error.message)
@@ -33,6 +62,44 @@ app.post('/product', async (req, res) => {
     
 
   })
+
+// Update a product in DB
+app.put('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);   
+        
+        //if we cannot find any product in the DB with same ID
+        if (!product) {
+            return res.status(404).json({message: 'Product with not found'})
+        } 
+        //load updated data and present it
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Delete a product from DB
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id); 
+
+        if(!product){
+            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+        }
+
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
+    
+})
 
 
 
